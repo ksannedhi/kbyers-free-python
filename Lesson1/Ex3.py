181e8947648b760e80492f012866b1aeff0a6b9a
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:54:56 +0530

    Create Ex3.py

---
+++ Lesson1/Ex3.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+ipv6_addr1 = "2001:db8:1234::1"
+IPV6_ADDR2 = "2001:db8:1234::2"
+ipV6_Addr3 = "2001:db8:1234::3"
+
+print("")
+print("Is first IPv6 is equal to second IPv6: {}".format(ipv6_addr1 == IPV6_ADDR2))
+print("Is first IPv6 is not equal to third IPv6: {}".format(ipv6_addr1 != ipV6_Addr3))
+print("")
